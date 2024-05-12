5c5c758d75dc308cb0a493feb4700de5269b419d
Timothy Chai <s10256132@connect.np.edu.sg>
2024-05-13 00:17:02 +0800

    Week04 Practical

---
+++ Week04/books-api-mvc-db/dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    user: "booksapi_user",
+    password: "12N392432p06",
+    server: "localhost",
+    database: "bed_db",
+    trustServerCertificate: true,
+    options: {
+        port: 1433, // Default SQL Server port
+        connectionTimeout: 60000, // Connection timeout in millisecond
+    },
+};
